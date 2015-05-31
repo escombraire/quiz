0e3405e6e00938e5ba12e25876decef176b689e8
Javier Homs <javier.homs@gmail.com>
2015-05-31 17:00:25 +0200

    Se ha añadido layout.ejs

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
 
 var app = express();
+var parcial = partials();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(parcial);
 
 app.use('/', routes);
 //app.use('/users', users);
